b03a3824dbe877f2327f8083e046258c436093f3
Gabriela <gabriela.priaro@gmail.com>
2024-11-06 15:09:58 +0000

    Adicionar arquivos de projeto.

---
+++ SomaMatrizes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SomaMatrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
